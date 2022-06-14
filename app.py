1ba70ed562129b40b75371e3afd80c315abdf141
Icode4passion <yogeerama@gmail.com>
2022-06-14 23:12:27 +0530

    code changed file renamed

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ print("Hello World")
 list=[1,2,3]
 for i in list:
     print(i)
+
+
+from flask import Flask 
+app=Flask(__new__)
+@app.route("/")
+def index():
+    return "Hello World"
+if __name__ == "__main__":
+    app.run(debug=True)
